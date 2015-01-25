478ab6bf0afd20aaa8fa8ca74528bb2ea71552e0
artpi <artpi@post.pl>
2015-01-25 12:40:37 +0100

    Config file

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    define('OAUTH_CALLBACK_URL', "http://tools.artpi.pl/oauth/callback.php");
+    define('EVERNOTE_API_PATH', "./evernote-sdk-php/lib");
+    define('EVERNOTE_TOKEN', "your token");
+    define('EVERNOTE_API_PATH', "./");
+    
+    define('DB', 'your db connection string');
+    define('DB_USER', '');
+    define('DB_PASS', '');
+
+
+    define('JOURNAL_GOOGLE_CALENDARS', 'calendar1 id;calendar2 id');
+    define('EVERNOTE_NOTEBOOK', "your guid");
+    define('EVERNOTE_JOURNAL_TEMPLATE', "your guid");
+    define('EVERNOTE_TODO_TAG', "your guid");
+
+    define('MISFIT_TOKEN', "your misfit.com token");
+
+
+?>
